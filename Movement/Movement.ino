55f0fc097d847b8e300c31682e1ceb4fc8fc6ade
IanTheWrong <55566118+IanTheWrong@users.noreply.github.com>
2023-10-14 17:21:42 -0400

    Deleted Buggy Turn functions

--- Movement/Movement.ino
+++ Movement/Movement.ino
@@ -8,28 +8,6 @@ MPU6050_getdata AppMPU6050getdata;
 int time1 = 0;
 int time2 = 0;
 ConquerorCarMotionControl status = Forward;
-static void turnLeft(float Yaw, int Speed){
-  if(Yaw > -90){
-    ApplicationFunctionSet_ConquerorCarMotionControl(Left /*direction*/, Speed /*speed*/);
-  }
-  else if(Yaw < -92){
-    ApplicationFunctionSet_ConquerorCarMotionControl(Right /*direction*/, Speed /*speed*/);
-  }
-  else{
-    ApplicationFunctionSet_ConquerorCarMotionControl(stop_it /*direction*/, Speed /*speed*/);
-  }
-}
-static void turnRight(float Yaw, int Speed){
-  if(Yaw < 90){
-    ApplicationFunctionSet_ConquerorCarMotionControl(Left /*direction*/, Speed /*speed*/);
-  }
-  else if(Yaw > 92{
-    ApplicationFunctionSet_ConquerorCarMotionControl(Right /*direction*/, Speed /*speed*/);
-  }
-  else{
-    ApplicationFunctionSet_ConquerorCarMotionControl(stop_it /*direction*/, Speed /*speed*/);
-  }
-}
 
 
 void setup() {
@@ -45,7 +23,7 @@ void setup() {
 void loop() {
   static float YawC;
   AppMPU6050getdata.MPU6050_dveGetEulerAngles(&YawC);
+  lround(YawC);
   Serial.println(YawC);
-  turnLeft(YawC, 60);
 
 }
